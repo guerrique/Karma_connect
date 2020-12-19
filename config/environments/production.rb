c62155ec2ee243a3fe3a1e9c78260b2cfa9f087a
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-12-19 20:13:25 +0800

    changed adresses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://www.karmaconnect.live/cable'
-  config.web_socket_server_url = 'wss://www.karmaconnect.live/cable'
-  config.action_cable.allowed_request_origins = ['https://www.karmaconnect.live', 'https://karmaconnect.herokuapp.com']
+  config.action_cable.url = 'wss://https://connectkarma.herokuapp.com/cable'
+  config.web_socket_server_url = 'wss://https://connectkarma.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://connectkarma.herokuapp.com/']
 
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
